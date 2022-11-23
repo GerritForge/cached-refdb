167490520dd5f8ef845482b6f1487cac66e3226e
Jacek Centkowski <geminica.programs@gmail.com>
2022-11-23 08:27:36 +0100

    Cache refs/... resolutions
    When `Repository.resolve` is called it recognizes if the string is a ref
    reference and returns its ObjectId. Cache all requests to `refs/...` so
    that reading from repository is avoided. Note that ref tip's parent
    references are not cached.
    Note that `PrologCompiler.call` calls `Repository.resolve` on
    `refs/meta/config` and that should be improved.
    
    Add integration test that compares the `resolve` method output with the
    JGit's one for the following patterns:
    * HEAD
    * SHA-1: a complete or abbreviated SHA-1
    * refs/...: a complete reference name
    * refs/.../^: first parent of the complete reference name's tip commit
    * short-name: a short reference name under refs/heads, refs/tags, or
      refs/remotes namespace
    * tag-NN-gABBREV: output from describe, parsed by treating ABBREV as an
      abbreviated > SHA-1.
    * id^: first parent of commit id, this is the same as id^1
    * id^{commit}: ensure id is a commit
    
    Bug: Issue 15556
    Change-Id: I9a722d55b105a27ceda7e0ecbec1ce886ff896f5

--- src/main/java/com/googlesource/gerrit/plugins/cachedrefdb/CachedRefRepository.java
+++ src/main/java/com/googlesource/gerrit/plugins/cachedrefdb/CachedRefRepository.java
@@ -14,6 +14,7 @@
 
 package com.googlesource.gerrit.plugins.cachedrefdb;
 
+import com.google.gerrit.entities.RefNames;
 import com.google.gerrit.server.git.DelegateRepository;
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
@@ -143,6 +144,13 @@ class CachedRefRepository extends DelegateRepository {
   public ObjectId resolve(String revstr)
       throws AmbiguousObjectException, IncorrectObjectTypeException, RevisionSyntaxException,
           IOException {
+    if (isCacheableReference(revstr)) {
+      Ref ref = refDb.exactRef(revstr);
+      if (ref != null) {
+        return ref.getLeaf().getObjectId();
+      }
+    }
+
     return delegate.resolve(revstr);
   }
 
@@ -354,5 +362,9 @@ class CachedRefRepository extends DelegateRepository {
 
   public String getProjectName() {
     return projectName;
+  }
+
+  private boolean isCacheableReference(String ref) {
+    return ref.startsWith(RefNames.REFS) && !(ref.contains("^") || ref.contains("~"));
   }
 }
