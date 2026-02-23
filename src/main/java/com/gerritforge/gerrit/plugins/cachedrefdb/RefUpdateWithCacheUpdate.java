2ead108eed5caf258a4fba56756046119c7bfbc5
Daniele Sassoli <danielesassoli@gmail.com>
2026-02-23 14:24:37 +0000

    Introduce ref_names_by_object_id cache to cache getTipsWithSha1
    CachedRefDatabase.getTipsWithSha1(ObjectId) previously delegated
    directly to the underlying RefDatabase, performing a full ref scan on
    every call.
    
    Introduce ref_names_by_object_id in order to reduce access to disk and
    speed up reverse index look-ups from objectIds to refs.
    
    Issue: 472359958
    Change-Id: Ie744d4309a72329c3f6329eebde7672e6d0162fb

--- src/main/java/com/gerritforge/gerrit/plugins/cachedrefdb/RefUpdateWithCacheUpdate.java
+++ src/main/java/com/gerritforge/gerrit/plugins/cachedrefdb/RefUpdateWithCacheUpdate.java
@@ -224,6 +224,7 @@ class RefUpdateWithCacheUpdate extends RefUpdate {
     try {
       if (SUCCESSFUL_UPDATES.contains(r)) {
         refsCache.evict(repo.getProjectName(), getName());
+        refsCache.removeRefFromObjectIdCache(repo.getProjectName(), getName(), getOldObjectId());
       }
       return r;
     } catch (ExecutionException e) {
@@ -232,9 +233,18 @@ class RefUpdateWithCacheUpdate extends RefUpdate {
   }
 
   private Result refreshCachesOnSuccessfulUpdate(Result r) throws IOException {
-    if (SUCCESSFUL_UPDATES.contains(r)) {
-      refsCache.updateRef(repo.getProjectName(), getName(), delegateRefDb);
+    try {
+      if (SUCCESSFUL_UPDATES.contains(r)) {
+        refsCache.updateRef(repo.getProjectName(), getName(), delegateRefDb);
+        refsCache.removeRefFromObjectIdCache(repo.getProjectName(), getName(), getOldObjectId());
+        Ref updated = refsCache.get(repo.getProjectName(), getName(), delegateRefDb);
+        if (updated != null) {
+          refsCache.addRefToObjectIdCache(repo.getProjectName(), updated);
+        }
+      }
+      return r;
+    } catch (ExecutionException e) {
+      throw new IOException(e);
     }
-    return r;
   }
 }
