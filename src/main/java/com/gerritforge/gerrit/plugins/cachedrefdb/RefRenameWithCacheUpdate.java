409814d35e4b8480d4e3c86e6ff5b060a3266ac2
Daniele Sassoli <danielesassoli@gmail.com>
2026-02-23 14:24:37 +0000

    Introduce ref_names_by_object_id cache to cache getTipsWithSha1
    CachedRefDatabase.getTipsWithSha1(ObjectId) previously delegated
    directly to the underlying RefDatabase, performing a full ref scan on
    every call.
    
    Introduce ref_names_by_object_id in order to reduce access to disk and
    speed up reverse index look-ups from objectIds to refs.
    
    Issue: 472359958
    Change-Id: Ie744d4309a72329c3f6329eebde7672e6d0162fb

--- src/main/java/com/gerritforge/gerrit/plugins/cachedrefdb/RefRenameWithCacheUpdate.java
+++ src/main/java/com/gerritforge/gerrit/plugins/cachedrefdb/RefRenameWithCacheUpdate.java
@@ -18,6 +18,7 @@ import java.io.IOException;
 import java.util.EnumSet;
 import java.util.concurrent.ExecutionException;
 import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.RefRename;
 import org.eclipse.jgit.lib.RefUpdate;
 import org.eclipse.jgit.lib.RefUpdate.Result;
@@ -99,7 +100,11 @@ class RefRenameWithCacheUpdate extends RefRename {
     if (SUCCESSFUL_RENAMES.contains(r)) {
       String projectName = repo.getProjectName();
       try {
-        refsCache.renameRef(projectName, src.getRef(), repo.getCachedRefDatabase().getDelegate().exactRef(dst.getName()));
+        Ref dstRef = repo.getCachedRefDatabase().getDelegate().exactRef(dst.getName());
+        refsCache.renameRef(projectName, src.getRef(), dstRef);
+        refsCache.removeRefFromObjectIdCache(
+            projectName, src.getRef().getName(), src.getRef().getObjectId());
+        refsCache.addRefToObjectIdCache(projectName, dstRef);
       } catch (ExecutionException e) {
         logger.atWarning().log(
             "Cannot update cache for project %s, source ref %s, dest ref %s",
