b40431599e9b40a6067507b9f719a9968e54c981
Daniele Sassoli <danielesassoli@gmail.com>
2026-03-09 17:46:23 +0000

    Add RefTernarySearchTree for Ref-keyed storage
    Ref-specialised subclass that will eventually provide utility methods to
    query the TernarySearchTree by object id.
    
    Change-Id: I0061ab4a9b7a5a2fee396df9a30e2fa43b3de4b4

---
+++ src/main/java/com/gerritforge/gerrit/plugins/cachedrefdb/RefTernarySearchTree.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2026 GerritForge, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v. 1.0 which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+package com.gerritforge.gerrit.plugins.cachedrefdb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jgit.lib.AnyObjectId;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Ref;
+
+/**
+ * A {@link TernarySearchTree} specialised for {@link Ref} values, keyed by ref
+ * name, with the ability to look up refs by the {@link ObjectId} they point to.
+ *
+ * @since 6.5
+ */
+public class RefTernarySearchTree extends TernarySearchTree<Ref> {
+
+}
