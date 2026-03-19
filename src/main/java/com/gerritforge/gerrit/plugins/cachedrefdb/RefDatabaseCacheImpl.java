716cdd7bbb517a1a998585419b2f6bf8d878295c
Daniele Sassoli <danielesassoli@gmail.com>
2026-03-19 13:11:49 +0000

    Use local version of TernarySearchTree
    This is a copy of TernarySearchTree as of the commit SHA at [1].
    
    Given the current issues with migrating the latest version of JGit to
    Gerrit(See conversation at [2]) we are momentarily left with no solution
    but to bring the TernarySearchTree definition within the plugin while we
    wait to be able to rely on the latest updates to JGit.
    
    [1] c0d5111e99a9e91db000c70806f49100c72196a6
    [2] https://review.gerrithub.io/c/eclipse-jgit/jgit/+/1233366/comments/fa1c0fa2_e302817e
    
    Change-Id: Ibf65074231abd0d12d6d998d1547d5a2214b8c97

--- src/main/java/com/gerritforge/gerrit/plugins/cachedrefdb/RefDatabaseCacheImpl.java
+++ src/main/java/com/gerritforge/gerrit/plugins/cachedrefdb/RefDatabaseCacheImpl.java
@@ -26,7 +26,7 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.Lock;
-import org.eclipse.jgit.internal.storage.memory.TernarySearchTree;
+import com.gerritforge.gerrit.plugins.cachedrefdb.TernarySearchTree;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.RefDatabase;
 
